e98d4bbdf0415131efbd1da0399d2ebc763bb1aa
bhagwatkhandekar <bhagwatkhandekar312@gmail.com>
2024-08-20 08:54:05 +0530

    Image attacchment added for product model using carrierwave gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
 
- def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "description", "email", "id", "name", "price", "updated_at"]
-  end
+   mount_uploader :image, ImageUploader
+
+   def self.ransackable_attributes(auth_object = nil)
+      ["created_at", "description", "email", "id", "name", "price", "updated_at", "image"]
+   end
   
 end
